1c24c3f249f9b53dd7c73680d84002e9c1479c3f
Adriel Alberto Martins Correia da Silva <martins@debian-BULLSEYE-live-builder-AMD64>
2021-09-24 11:10:35 -0300

    pythagoras

---
+++ pythagoras.py
@@ -0,0 +1,10 @@
+import math
+
+def pythagoras(x,y):
+    """To get the hipotenuse of a right triangle"""
+    """Hipotenusa de um triângulo retângulo"""
+    return math.sqrt(math.pow(x,2) + math.pow(y,2))
+
+
+
+print(pythagoras(3,4))
